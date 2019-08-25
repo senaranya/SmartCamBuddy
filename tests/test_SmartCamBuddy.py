de72503149c7c9beb0f6237b2d0abfcd2d12728c
Aranya Sen <sen.aranya@gmail.com>
2019-08-25 20:42:35 +0530

    Add test skeleton

--- tests/test_SmartCamBuddy.py
+++ tests/test_SmartCamBuddy.py
@@ -1,15 +1,20 @@
 import smarcambuddy
 import pytest
 
+def test_trigger_function_triggers_a_photo():
+    photoPath = smarcambuddy.trigger()
+    # assert photoPath exists
+    # assert photoPath contains a valid photo
+    # delete photoPath
 
-def fizzBuzz(value):
-    return str(value)
-
-def test_returns1With1PassedIn():
-    retVal = fizzBuzz(1)
-    assert retVal == "1"
-    assert False
-
-def test_returns2With2PassedIn():
-    retVal = fizzBuzz(2)
-    assert retVal == "2"
+# def fizzBuzz(value):
+#     return str(value)
+#
+# def test_returns1With1PassedIn():
+#     retVal = fizzBuzz(1)
+#     assert retVal == "1"
+#     assert False
+#
+# def test_returns2With2PassedIn():
+#     retVal = fizzBuzz(2)
+#     assert retVal == "2"
